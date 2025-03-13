097cca3deb39bf9c0ce03c1d0a342db169afc292
Jace H <76619307+Cassa-sys@users.noreply.github.com>
2025-03-13 12:33:00 +0000

    Retrofit Website to the Marketing Template

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,17 @@
+'use client'
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import NextLink from 'next/link';
-import ProTip from '@/components/ProTip';
-import Copyright from '@/components/Copyright';
+import Hero from '@/components/Hero';
+import LogoCollection from '@/components/LogoCollection';
+import Highlights from '@/components/Highlights';
+import Pricing from '@/components/Pricing';
+import Features from '@/components/Features';
+import Testimonials from '@/components/Testimonials';
+import FAQ from '@/components/FAQ';
+import Footer from '@/components/Footer';
+import Divider from '@mui/material/Divider';
+
 
 export default function Home() {
   return (
@@ -19,14 +25,21 @@ export default function Home() {
           alignItems: 'center',
         }}
       >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js App Router example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary" component={NextLink}>
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
+      <Hero />
+      <div>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </div>
       </Box>
     </Container>
   );
